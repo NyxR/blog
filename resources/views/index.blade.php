0206ad234f280a1364570f29ddc8357b5fbc24ec
NyxR <42943018+NyxR@users.noreply.github.com>
2019-03-30 21:19:54 +0100

    Admin Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/landingPage.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/lightbox.min.css">
+    <link rel="stylesheet" type="" href="../fontawesome-free-5.3.1-web/css/all.css">
+    <script src="../public/js/jquery.min.js"></script>
+    <script src="../public/js/lightbox-plus-jquery.min.js"></script>
+     
+    <title>HYPERDEV</title>
+    
+</head>
+<body>
+    <header id="#home">
+        <div class="navbar">
+            <div class="logo"><img src="../resources/img/HyperDevVectFin.png"></div>
+            <nav>
+                <ul>
+                    <li><a href="#" class="active">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Team</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+            </nav>
+            <div class="menu-toggle"><i class="fa fa-bars" aria-hidden="true"></i></div>
+        </div>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="banner" style="background-image: url('../resources/img/bg5.jpg');"></div>
+                    <div class="carousel-caption">
+                        <h2 class="animated bounceInRight" style="animation-delay: 1s">We are <span style="color: rgb(215,66,66);">Hyper</span><span style="color: #333;">Dev</span></h2>
+                        <h3 class="animated bounceInLeft" style="animation-delay: 2s">Web Design and Development</h3>
+                        <p class="animated bounceInRight" style="animation-delay: 3s"><a href="#">Get Started</a></p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="banner" style="background-image:  url('../resources/img/bg3.jpg');"></div>
+                    <div class="carousel-caption">
+                        <h2 class="animated fadeIn" style="animation-delay: 1s">We are <span style="color: rgb(215,66,66);">Creative</span></h2>
+                        <h3 class="animated fadeIn" style="animation-delay: 2s">Web Design and Development</h3>
+                        <p class="animated fadeIn" style="animation-delay: 3s"><a href="#">Get Started</a></p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </header>
+    
+    <section id="about" class="about-section slanted">
+        <h1>About us</h1>
+        <div class="container about-container">
+            <div class="row">
+                <div class="col-sm-12 col-md-4 col-lg-5"><img src="../resources/img/HyperDevVectFin.png" class="img-fluid"></div>
+                <div class="col-sm-12 col-md-8 col-lg-7">
+                    <div class="about-inner">
+                        <h2><span style="color: rgb(215,66,66)">Hyper<span style="color: #333">Dev</span></span></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit ame5t, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor it amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br> <a href="#">Read More ...</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </section>
+    
+    <section id="services" class="services-section slanted">
+        <div class="container">
+            <div class="services-inner">
+                <h1>Our Services</h1>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-lg-3 text-center services-content">
+                    <div class="icon">
+                        <i class="fa fa-desktop"></i>
+                    </div>
+                    <h3>Web Development</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center services-content">
+                    <div class="icon">
+                        <i class="fa fa-tablet"></i>
+                    </div>
+                    <h3>Mobile Apps</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center services-content">
+                    <div class="icon">
+                        <i class="fa fa-mobile"></i>
+                    </div>
+                    <h3>UI/UX Design</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center services-content">
+                    <div class="icon">
+                        <i class="fa fa-paint-brush"></i>
+                    </div>
+                    <h3>Graphism</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            
+            </div>
+        </div>
+    
+    </section>
+    
+
+    <section id="work" class="work-section">
+        <div class="container">
+            <div class="work-inner">
+                <h1>Our Works</h1>
+                <p>Checkout Our Lastest Work</p>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="single-portfolio">
+                        <a href="../resources/img/img1.jpg" data-lightbox="portfolio"><img src="../resources/img/img1.jpg"></a>
+                        <div class="portfolio-caption">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem Ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="single-portfolio">
+                        <a href="../resources/img/img2.jpg" data-lightbox="portfolio"><img src="../resources/img/img2.jpg"></a>
+                        <div class="portfolio-caption">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem Ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="single-portfolio">
+                        <a href="../resources/img/img3.jpg" data-lightbox="portfolio"><img src="../resources/img/img3.jpg"></a>
+                        <div class="portfolio-caption">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem Ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="single-portfolio">
+                        <a href="../resources/img/img4.jpg" data-lightbox="portfolio"><img src="../resources/img/img4.jpg"></a>
+                        <div class="portfolio-caption">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem Ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="single-portfolio">
+                        <a href="../resources/img/img5.jpg" data-lightbox="portfolio"><img src="../resources/img/img5.jpg"></a>
+                        <div class="portfolio-caption">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem Ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-md-4">
+                    <div class="single-portfolio">
+                        <a href="../resources/img/img6.jpg" data-lightbox="portfolio"><img src="../resources/img/img6.jpg"></a>
+                        <div class="portfolio-caption">
+                            <h3>Lorem Ipsum</h3>
+                            <p>Lorem Ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    </section>
+    
+    <section id="contact" class="contact-section">
+        <div class="contact-inner">
+            <h1>Contact Us</h1>
+            
+            <form class="contact-form" action="landing.html" method="post">
+                <input type="text" class="contact-form-text" placeholder="Your name">
+                <input type="email" class="contact-form-text" placeholder="Your e-mail">
+                <input type="text" class="contact-form-text" placeholder="Your phone">
+                <textarea class="contact-form-text" placeholder="Your message"></textarea>
+                <input type="submit" class="contact-form-btn" value="Send">
+            </form>
+        </div>
+    </section>
+    
+    <footer id="footer">
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 footer-info">
+                        <h2><span style="color: rgb(215,66,66);">Hyper<span style="color: #000;">Dev</span></span></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class="col-lg-4 col-md-6 footer-links">
+                        <h2><span style="color: rgb(215,66,66);">Usefull <span style="color: #000;">Links</span></span></h2>
+                        <ul>
+                            <li><i class="fa fa-arrow-right links"></i><a href="#home">Home</a></li>
+                            <li><i class="fa fa-arrow-right links"></i><a href="#about">About</a></li>
+                            <li><i class="fa fa-arrow-right links"></i><a href="#services">Services</a></li>
+                            <li><i class="fa fa-arrow-right links"></i><a href="#portfolio">Portfolio</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 col-md-12 footer-contact">
+                        <h2><span style="color: rgb(215,66,66);">Follow <span style="color: #000;">Us</span></span></h2>
+                        <div class="footer-contact-inner">
+                            <a href="#"><i class="fab fa-facebook"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin"></i></a>
+                            <a href="#"><i class="fab fa-google"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 copyright">Copyright HyperDev 2019</div>
+                </div>
+            </div>
+        
+        </div>
+    
+    </footer>
+    
+    
+    <!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+<script src="../../bootstrap-4.1.3-dist/js/popper.min.js"></script>
+<script src="../../bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+<script src="../../bootstrap-4.1.3-dist/js/jquery-3.3.1.slim.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.menu-toggle').click(function(){
+            $('nav').toggleClass('active')
+        });
+        
+        $('.services-content').hover(function(){
+            $(this).children('.icon').toggleClass('hoverIcon');
+            $(this).children('h3').toggleClass('hover-services-content');
+        })
+    })
+</script>
+</body>
+</html>
